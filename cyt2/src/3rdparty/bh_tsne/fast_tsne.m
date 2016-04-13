8d34d4c34ea850e32eaa941f75d4d976e056afc5
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2016-04-13 12:19:13 -0400

    fast_tsne: fix ws

--- cyt2/src/3rdparty/bh_tsne/fast_tsne.m
+++ cyt2/src/3rdparty/bh_tsne/fast_tsne.m
@@ -3,8 +3,8 @@ function mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
 %
 %   mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
 %
-% Runs the C++ implementation of Barnes-Hut-SNE. The high-dimensional 
-% datapoints are specified in the NxD matrix X. The dimensionality of the 
+% Runs the C++ implementation of Barnes-Hut-SNE. The high-dimensional
+% datapoints are specified in the NxD matrix X. The dimensionality of the
 % datapoints is reduced to initial_dims dimensions using PCA (default = 50)
 % before t-SNE is performed. Next, t-SNE reduces the points to no_dims
 % dimensions. The perplexity of the input similarities may be specified
@@ -12,7 +12,7 @@ function mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
 % the trade-off parameter between speed and accuracy: theta = 0 corresponds
 % to standard, slow t-SNE, while theta = 1 makes very crude approximations.
 % Appropriate values for theta are between 0.1 and 0.7 (default = 0.5).
-% The variable alg determines the algorithm used for PCA. The default is set 
+% The variable alg determines the algorithm used for PCA. The default is set
 % to 'svd'. Other options are 'eig' or 'als' (see 'doc pca' for more details).
 % The function returns the two-dimensional data points in mappedX.
 %
@@ -23,7 +23,7 @@ function mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
 
 % Copyright (c) 2014, Laurens van der Maaten (Delft University of Technology)
 % All rights reserved.
-% 
+%
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are met:
 % 1. Redistributions of source code must retain the above copyright
@@ -34,19 +34,19 @@ function mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
 % 3. All advertising materials mentioning features or use of this software
 %    must display the following acknowledgement:
 %    This product includes software developed by the Delft University of Technology.
-% 4. Neither the name of the Delft University of Technology nor the names of 
-%    its contributors may be used to endorse or promote products derived from 
+% 4. Neither the name of the Delft University of Technology nor the names of
+%    its contributors may be used to endorse or promote products derived from
 %    this software without specific prior written permission.
 %
 % THIS SOFTWARE IS PROVIDED BY LAURENS VAN DER MAATEN ''AS IS'' AND ANY EXPRESS
-% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
-% EVENT SHALL LAURENS VAN DER MAATEN BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
-% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
-% IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+% EVENT SHALL LAURENS VAN DER MAATEN BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+% IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 % OF SUCH DAMAGE.
 
 
@@ -65,19 +65,19 @@ function mappedX = fast_tsne(X, no_dims, initial_dims, perplexity, theta, alg)
     if ~exist('alg', 'var') || isempty(alg)
         alg = 'svd';
     end
-    
+
     % Perform the initial dimensionality reduction using PCA
     X = double(X);
     X = bsxfun(@minus, X, mean(X, 1));
     M = pca(X,'NumComponents',initial_dims,'Algorithm',alg);
     X = X * M;
-    
+
     % Run the fast diffusion SNE implementation
     tsne_path = which('fast_tsne');
     tsne_path = fileparts(tsne_path);
     write_data(X, no_dims, theta, perplexity);
     tic, system(fullfile(tsne_path,'./bh_tsne')); toc
-    [mappedX, landmarks, costs] = read_data;   
+    [mappedX, landmarks, costs] = read_data;
     landmarks = landmarks + 1;              % correct for Matlab indexing
     delete('data.dat');
     delete('result.dat');
@@ -88,24 +88,24 @@ end
 function write_data(X, no_dims, theta, perplexity)
     [n, d] = size(X);
     h = fopen('data.dat', 'wb');
-	fwrite(h, n, 'integer*4');
-	fwrite(h, d, 'integer*4');
+    fwrite(h, n, 'integer*4');
+    fwrite(h, d, 'integer*4');
     fwrite(h, theta, 'double');
     fwrite(h, perplexity, 'double');
-	fwrite(h, no_dims, 'integer*4');
+    fwrite(h, no_dims, 'integer*4');
     fwrite(h, X', 'double');
-	fclose(h);
+    fclose(h);
 end
 
 
 % Reads the result file from the fast t-SNE implementation
 function [X, landmarks, costs] = read_data
     h = fopen('result.dat', 'rb');
-	n = fread(h, 1, 'integer*4');
-	d = fread(h, 1, 'integer*4');
-	X = fread(h, n * d, 'double');
+    n = fread(h, 1, 'integer*4');
+    d = fread(h, 1, 'integer*4');
+    X = fread(h, n * d, 'double');
     landmarks = fread(h, n, 'integer*4');
     costs = fread(h, n, 'double');      % this vector contains only zeros
     X = reshape(X, [d n])';
-	fclose(h);
+    fclose(h);
 end
